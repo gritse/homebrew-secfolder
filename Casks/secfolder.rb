28f1e583f7a6ae71fb12abdf4cfa94cd19d6d1a6
gritse <24323376+gritse@users.noreply.github.com>
2025-01-16 16:26:52 +0100

    Update secfolder.rb

---
+++ Casks/secfolder.rb
@@ -0,0 +1,66 @@
+cask 'secfolder' do
+  version "0.1.2"
+  sha256 :no_check
+
+  url "https://secfolder.blob.core.windows.net/test/SecFolder-#{version}.zip"
+  name 'SecFolder'
+  homepage 'https://secfolder.net'
+
+  depends_on cask: "santa"
+  depends_on macos: ">= :sonoma"
+  
+  app 'SecFolder.app'
+
+  preflight do
+    system_command 'mkdir',
+                   args: ['-p', '/Library/Preferences/SecFolder'],
+                   sudo: true
+    system_command 'mkdir',
+                   args: ['-p', File.expand_path('~/SecretDocuments')],
+                   sudo: false
+    system_command 'cp',
+                   args: [
+                     staged_path.join('readme.txt'),
+                     File.expand_path('~/SecretDocuments/readme.txt')
+                   ],
+                   sudo: false
+  end
+
+  postflight do
+    system_command "codesign",
+                   args: [
+                     "--deep", "-f", "-s", "-", "--verbose=4", "/Applications/SecFolder.app"
+                   ],
+                   sudo: true
+    system_command 'cp',
+                   args: [
+                     staged_path.join('com.warsaw.SecFolder.Helper.plist'),
+                     '/Library/LaunchDaemons/com.warsaw.SecFolder.Helper.plist'
+                   ],
+                   sudo: true
+    system_command "launchctl",
+                   args: [
+                     "load", "-w", "/Library/LaunchDaemons/com.warsaw.SecFolder.Helper.plist"
+                   ],
+                   sudo: true
+    system_command 'open',
+                   args: [staged_path.join('com.google.santa.SecFolder.mobileconfig')],
+                   sudo: false
+  end
+  
+  caveats do
+  	license "https://secfolder.net/terms.html"
+  end
+  
+  uninstall delete:		['/Applications/SecFolder.app', '/Library/Preferences/SecFolder'],
+            quit:    	['com.warsaw.SecFolder', 'com.warsaw.SecFolder.Helper'],
+            launchctl: 	'com.warsaw.SecFolder.Helper'
+            
+
+  zap trash: [
+    '~/Library/Application Support/SecFolder',
+    '~/Library/Caches/com.warsaw.SecFolder',
+    '~/Library/Preferences/com.warsaw.SecFolder.plist',
+    '~/Library/Saved Application State/com.warsaw.SecFolder.savedState'
+  ]
+end
