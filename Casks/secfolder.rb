d0f16e25db4f6b52d5beec95238f8e2e2a8426a1
gritse <24323376+gritse@users.noreply.github.com>
2025-01-23 20:09:12 +0100

    Update secfolder.rb

--- Casks/secfolder.rb
+++ Casks/secfolder.rb
@@ -1,5 +1,5 @@
 cask 'secfolder' do
-  version "0.1.2"
+  version "0.1.3"
   sha256 :no_check
 
   url "https://secfolder.blob.core.windows.net/brew/SecFolder-#{version}.zip"
@@ -24,14 +24,19 @@ cask 'secfolder' do
                      File.expand_path('~/SecretDocuments/readme.txt')
                    ],
                    sudo: false
+    system_command "xattr",
+                   args: [
+                     "-d", "com.apple.quarantine", staged_path.join('SecFolder.app')
+                   ],
+                   sudo: true
+      system_command "codesign",
+                   args: [
+                     "--deep", "-f", "-s", "-", "--verbose=4", staged_path.join('SecFolder.app')
+                   ],
+                   sudo: true
   end
 
   postflight do
-    system_command "codesign",
-                   args: [
-                     "--deep", "-f", "-s", "-", "--verbose=4", "/Applications/SecFolder.app"
-                   ],
-                   sudo: true
     system_command 'cp',
                    args: [
                      staged_path.join('com.warsaw.SecFolder.Helper.plist'),
@@ -52,15 +57,18 @@ cask 'secfolder' do
   	license "https://secfolder.net/terms.html"
   end
   
-  uninstall delete:		['/Applications/SecFolder.app', '/Library/Preferences/SecFolder'],
+  uninstall delete:		['/Applications/SecFolder.app'],
             quit:    	['com.warsaw.SecFolder', 'com.warsaw.SecFolder.Helper'],
             launchctl: 	'com.warsaw.SecFolder.Helper'
             
 
   zap trash: [
     '~/Library/Application Support/SecFolder',
+    '~/Library/Application Support/com.warsaw.SecFolder',
     '~/Library/Caches/com.warsaw.SecFolder',
     '~/Library/Preferences/com.warsaw.SecFolder.plist',
-    '~/Library/Saved Application State/com.warsaw.SecFolder.savedState'
+    '~/Library/Saved Application State/com.warsaw.SecFolder.savedState',
+    '/Users/Shared/SecFolder',
+    '/Library/Preferences/SecFolder'
   ]
 end
